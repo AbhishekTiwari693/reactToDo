f0c662603d226a62ca294709f8df1b7be5b6eeaf
Abhishek Tiwari <abhishektiwari62046@gmail.com>
2020-11-05 14:13:10 +0530

    Updates

---
+++ precache-manifest.4d77829e16db5e3023e882df14fa1a8f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a60a1309d65a69f7949d708a23dbb4",
+    "url": "/reactToDo/index.html"
+  },
+  {
+    "revision": "3ff1c6048d6901480a19",
+    "url": "/reactToDo/static/css/main.70019ed8.chunk.css"
+  },
+  {
+    "revision": "ef457d619cab0129cf17",
+    "url": "/reactToDo/static/js/2.12d12961.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactToDo/static/js/2.12d12961.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ff1c6048d6901480a19",
+    "url": "/reactToDo/static/js/main.ffee2ae1.chunk.js"
+  },
+  {
+    "revision": "2d6082a035b1ada3b629",
+    "url": "/reactToDo/static/js/runtime-main.bd37f10f.js"
+  }
+]);
